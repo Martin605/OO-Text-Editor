06f45974550a993f29b6ebd12012fa3270d9bd5f
Martin Leung <Martin_0605@outlook.com>
2021-10-31 23:45:01 +0800

    Update ToolMenu.java

--- src/editor/gui/menuBar/ToolMenu.java
+++ src/editor/gui/menuBar/ToolMenu.java
@@ -3,25 +3,13 @@ package editor.gui.menuBar;
 import javax.swing.*;
 
 import editor.TextEditor;
-import editor.file.fileList.iterator.FLList;
 import editor.gui.frame.*;
 
 import java.awt.event.ActionEvent;
-import java.io.File;
 
 public class ToolMenu extends MenuItem {
     ToolMenu(TextEditor te) {
         super("Tool");
-        this.menuItem.add(
-            new JMenuItem(
-                new AbstractAction("Run"){
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        te.getFileContollor().run();
-                    }
-                }
-            )
-        );
         this.menuItem.add(
             new JMenuItem(
                 new AbstractAction("Preview"){
