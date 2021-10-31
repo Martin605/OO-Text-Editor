d980208bb980a81a691980c48336df2ba4d7efe3
Martin Leung <Martin_0605@outlook.com>
2021-11-01 01:12:39 +0800

    remove search

--- src/editor/search/SContainer.java
+++
@@ -1,6 +0,0 @@
-package editor.search;
-
-// Container interface 
-public interface SContainer {
-	public SIterator getIterator();//Repository的getIterator實作出來
-}
--- src/editor/search/SIterator.java
+++
@@ -1,7 +0,0 @@
-package editor.search;
-
-// Iterator interface 有hasnext 跟 next 方法
-public interface SIterator {
-	public boolean hasNext();
-	public Object next();
-}
