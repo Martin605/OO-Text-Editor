9ebbd6a15bdc6f12c603cb9de323cc4adc9deaa2
EmmaLin1004 <78600914+EmmaLin1004@users.noreply.github.com>
2021-11-01 00:25:56 +0800

    add Composite notes

--- src/editor/file/fileList/FLFileItem.java
+++ src/editor/file/fileList/FLFileItem.java
@@ -2,7 +2,8 @@ package editor.file.fileList;
 
 import java.io.File;
 
-// Composite Pattern [L]
+// Composite Pattern
+//Primitive Component (Leaf)
 public class FLFileItem implements FLFile {
 
     private String name;
@@ -10,19 +11,23 @@ public class FLFileItem implements FLFile {
 
     protected FLFileItem(String path) {
         File file = new File(path);
+        //為了使用File的getName()(File為JAVA內建)，來取得檔案名稱
+
         this.path = path;
         this.name = file.getName();
+        //設定name為檔案名稱
     }
 
-    public String get_name() {
+    public String get_name() { //回傳名稱
         return this.name;
     };
 
-    public String get_type() {
+    public String get_type() { //回傳類型
         return "file";
+        //回傳"檔案"類型
     };
 
-    public String get_path() {
+    public String get_path() { //回傳路徑
         return this.path;
     };
 
