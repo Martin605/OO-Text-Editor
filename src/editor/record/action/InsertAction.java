55d0bac91e9cf8d9b9456d1e38bb0ef1437a0431
EASONftytfu <A2489372454@gmail.com>
2021-12-15 21:09:03 +0800

    Command註解

--- src/editor/record/action/InsertAction.java
+++ src/editor/record/action/InsertAction.java
@@ -11,7 +11,7 @@ import javax.swing.text.BadLocationException;
 import editor.TextEditor;
 
 public class InsertAction implements RecordAction{
-	private JFileChooser fileChooser = new JFileChooser();//����ɮת�����
+	private JFileChooser fileChooser = new JFileChooser();//選擇檔案的視窗
     private int fileId;
            
 
@@ -19,7 +19,7 @@ public class InsertAction implements RecordAction{
 	public void action(TextEditor textEditor) {
 		String content;
 		File file;
-		fileChooser.setDialogTitle("Select a file that you want to Insert"); //�]�w�������D
+		fileChooser.setDialogTitle("Select a file that you want to Insert"); //設定視窗標題
 	    
 	    fileId = fileChooser.showOpenDialog(textEditor);	    
 	    
@@ -27,14 +27,14 @@ public class InsertAction implements RecordAction{
 	        file = fileChooser.getSelectedFile();  
 	        try {
 				content = new String (Files.readAllBytes( Paths.get(file.getAbsolutePath()) ));
-				//���o���e
+				//取得內容
 				try {
-					int len = textEditor.getTextArea().getText().length();//���o��������
-					int Elen = textEditor.getTextArea().getText(0, textEditor.getTextArea().getCaretPosition()).length();//���o���J�I�e������
-					int Alen = len-Elen-1;//���o���J�I�᪺����
-					String OContent = textEditor.getTextArea().getText(0, textEditor.getTextArea().getCaretPosition());//���o���J�I�e�����e
-					String EContent =textEditor.getTextArea().getText(textEditor.getTextArea().getCaretPosition(),Alen);//���o���J�I�᪺���e
-					textEditor.getTextArea().setText(OContent + content+EContent);//�N�Ҧ���r���
+					int len = textEditor.getTextArea().getText().length();//取得全部長度
+					int Elen = textEditor.getTextArea().getText(0, textEditor.getTextArea().getCaretPosition()).length();//取得插入點前的長度
+					int Alen = len-Elen-1;//取得插入點後的長度
+					String OContent = textEditor.getTextArea().getText(0, textEditor.getTextArea().getCaretPosition());//取得插入點前的內容
+					String EContent =textEditor.getTextArea().getText(textEditor.getTextArea().getCaretPosition(),Alen);//取得插入點後的內容
+					textEditor.getTextArea().setText(OContent + content+EContent);//將所有文字顯示
 				
 				} catch (BadLocationException e) {
 					
